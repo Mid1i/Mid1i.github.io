c4de4695970d1bd99897006682d52a14e0e59fa4
Misha <100841904+Mid1i@users.noreply.github.com>
2023-02-11 20:08:25 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('.burger').click(function(event) {
+        $('.burger').toggleClass('active');
+    });
+
+    $('.pizza').click(function(event) {
+        $('.pizza').toggleClass('active');
+    });
+
+    $('.hot-dog').click(function(event) {
+        $('.hot-dog').toggleClass('active');
+    });
+
+    $('.cookies').click(function(event) {
+        $('.cookies').toggleClass('active');
+    });
+
+    $('.coke').click(function(event) {
+        $('.coke').toggleClass('active');
+    });
+
+    $('.fries').click(function(event) {
+        $('.fries').toggleClass('active');
+    });
+
+    $('.popcorn').click(function(event) {
+        $('.popcorn').toggleClass('active');
+    });
+
+    $('.sushi').click(function(event) {
+        $('.sushi').toggleClass('active');
+    });
+
+    $('.ice-cream').click(function(event) {
+        $('.ice-cream').toggleClass('active');
+    });
+});
