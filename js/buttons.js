8f62b518e0a384bbd2c174a5c15551397281a45e
Misha <100841904+Mid1i@users.noreply.github.com>
2023-02-15 23:50:38 +0300

    Add files via upload

--- js/buttons.js
+++ js/buttons.js
@@ -1,64 +1,11 @@
-$(document).ready(function() {
-    $('.button-burger').click(function(event) {
-        $('.burger').toggleClass('active');
-        $('.plus-burger').toggleClass('animateToRight');
-        $('.minus-burger').toggleClass('animateToLeft');
-        $('.button-burger').toggleClass('hide');
-    });
+document.addEventListener('click', function(event) {
+    let arg = event.target;
+    let id = arg.dataset.id;
 
-    $('.button-pizza').click(function(event) {
-        $('.pizza').toggleClass('active');
-        $('.plus-pizza').toggleClass('animateToRight');
-        $('.minus-pizza').toggleClass('animateToLeft');
-        $('.button-pizza').toggleClass('hide');
-    });
-
-    $('.button-hotdog').click(function(event) {
-        $('.hotdog').toggleClass('active');
-        $('.plus-hotdog').toggleClass('animateToRight');
-        $('.minus-hotdog').toggleClass('animateToLeft');
-        $('.button-hotdog').toggleClass('hide');
-    });
-
-    $('.button-cookies').click(function(event) {
-        $('.cookies').toggleClass('active');
-        $('.plus-cookies').toggleClass('animateToRight');
-        $('.minus-cookies').toggleClass('animateToLeft');
-        $('.button-cookies').toggleClass('hide');
-    });
-
-    $('.button-coke').click(function(event) {
-        $('.coke').toggleClass('active');
-        $('.plus-coke').toggleClass('animateToRight');
-        $('.minus-coke').toggleClass('animateToLeft');
-        $('.button-coke').toggleClass('hide');
-    });
-
-    $('.button-fries').click(function(event) {
-        $('.fries').toggleClass('active');
-        $('.plus-fries').toggleClass('animateToRight');
-        $('.minus-fries').toggleClass('animateToLeft');
-        $('.button-fries').toggleClass('hide');
-    });
-
-    $('.button-popcorn').click(function(event) {
-        $('.popcorn').toggleClass('active');
-        $('.plus-popcorn').toggleClass('animateToRight');
-        $('.minus-popcorn').toggleClass('animateToLeft');
-        $('.button-popcorn').toggleClass('hide');
-    });
-
-    $('.button-sushi').click(function(event) {
-        $('.sushi').toggleClass('active');
-        $('.plus-sushi').toggleClass('animateToRight');
-        $('.minus-sushi').toggleClass('animateToLeft');
-        $('.button-sushi').toggleClass('hide');
-    });
-
-    $('.button-icecream').click(function(event) {
-        $('.icecream').toggleClass('active');
-        $('.plus-icecream').toggleClass('animateToRight');
-        $('.minus-icecream').toggleClass('animateToLeft');
-        $('.button-icecream').toggleClass('hide');
-    });
-})
+    if (arg.classList.contains(`button-${id}`)) {
+        $(`.${id}`).toggleClass('active');
+        $(`.button-${id}`).toggleClass('hide');
+        $(`.plus-${id}`).toggleClass('animateToRight');
+        $(`.minus-${id}`).toggleClass('animateToLeft');
+    }
+});
--- js/counter.js
+++ js/counter.js
@@ -11,33 +11,54 @@ let cart = {
 }
 
 document.addEventListener('click', function(event) {
-
     let arg = event.target;
 
-    if (arg.classList.contains('plus-button')) {
+    if ((arg.classList.contains('plus-button')) || (arg.classList.contains('menu-button'))) {
         plusFunction(arg.dataset.id);
+        changeCounter(arg.dataset.id);
+        document.getElementById(`counter-${arg.dataset.id}`).classList.remove('hide');
+        document.getElementById('order-button').classList.remove('hide');
     }
 
     if (arg.classList.contains('minus-button')) {
         minusFunction(arg.dataset.id);
+        changeCounter(arg.dataset.id);
     }
+
+    checkOrder();
 });
 
 function plusFunction(id) {
     cart[id]++;
-    renderCart();
+    document.getElementById(`counter-${id}`).classList.remove('hide');
 }
 
 function minusFunction(id) {
     cart[id]--;
 
-    if (cart[id] < 0) {
+    if (cart[id] <= 0) {     
         $(`.${id}`).toggleClass('active');
+        $(`.button-${id}`).toggleClass('hide');
         $(`.plus-${id}`).toggleClass('animateToRight');
         $(`.minus-${id}`).toggleClass('animateToLeft');
-        $(`.button-${id}`).toggleClass('hide');
+        document.getElementById(`counter-${id}`).classList.add('hide');
+
         cart[id] = 0;
     }
+}
 
-    renderCart();
+function changeCounter(id) {
+    document.getElementById(`counter-${id}`).firstChild.data = cart[id];
+}
+
+function checkOrder() {
+    let check = 0;
+
+    for (var id in cart) {
+        if (cart[id] > 0) {check += 1;}
+    }
+
+    if (check == 0) {
+        document.getElementById('order-button').classList.add('hide');
+    }
 }
